4d9d1ec78fa90ec45202e3c47363f3a9f19909ce
= <vincent.qi@hotmail.com>
2021-10-09 20:52:19 -0700

    Delete Files

--- Natural Selection Simulator/Library/Il2cppBuildCache/WebGL/il2cppOutput/lumpedcpp/Lump_libil2cpp_debugger.cpp
+++
@@ -1,2 +0,0 @@
-#include "il2cpp-config.h"
-#include "/Applications/Unity/Hub/Editor/2019.2.18f1/2021.1.12f1/Unity.app/Contents/il2cpp/libil2cpp/debugger/il2cpp-stubs.cpp"
--- Natural Selection Simulator/Library/Il2cppBuildCache/WebGL/il2cppOutput/lumpedcpp/Lump_libil2cpp_mono.cpp
+++
@@ -1,7 +0,0 @@
-#include "il2cpp-config.h"
-#include "/Applications/Unity/Hub/Editor/2019.2.18f1/2021.1.12f1/Unity.app/Contents/il2cpp/libil2cpp/mono/MonoPosixHelper.cpp"
-#include "/Applications/Unity/Hub/Editor/2019.2.18f1/2021.1.12f1/Unity.app/Contents/il2cpp/libil2cpp/mono/ThreadPool/ThreadPoolMonitorThread.cpp"
-#include "/Applications/Unity/Hub/Editor/2019.2.18f1/2021.1.12f1/Unity.app/Contents/il2cpp/libil2cpp/mono/ThreadPool/ThreadPoolWorkerThread.cpp"
-#include "/Applications/Unity/Hub/Editor/2019.2.18f1/2021.1.12f1/Unity.app/Contents/il2cpp/libil2cpp/mono/ThreadPool/threadpool-ms-io-poll.cpp"
-#include "/Applications/Unity/Hub/Editor/2019.2.18f1/2021.1.12f1/Unity.app/Contents/il2cpp/libil2cpp/mono/ThreadPool/threadpool-ms-io.cpp"
-#include "/Applications/Unity/Hub/Editor/2019.2.18f1/2021.1.12f1/Unity.app/Contents/il2cpp/libil2cpp/mono/ThreadPool/threadpool-ms.cpp"
